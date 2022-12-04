63be7f7398f709817afc726ab04926203a6b1ca9
Pontus Nagy <p_nagy@icloud.com>
2022-12-04 23:38:32 +0100

    finished day3B and day4

--- lib/day_3/day_3.ml
+++ lib/day_3/day_3.ml
@@ -1,24 +1,72 @@
 open ContainersLabels
+open CCFun
+
+let test_input =
+  [
+    "vJrwpWtwJgWrhcsFMMfFFhFp";
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
+    "PmmdzqPrVvPwwTWBwg";
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
+    "ttgJtRGJQctTZtZT";
+    "CrZsJsPPZsGzwwsLwLmpwMDw";
+  ]
+
+let priority c =
+  let c' = Char.code c in
+  if c' > 96 then c' - 96 else c' - 38
+
+module CSet = Set.Make (Char)
 
 module A = struct
-  let solve _l = 1
+  let solve_aux input =
+    let half = String.length input / 2 in
+    let parsed_input = CCString.to_list input in
+    let rec scan_comparment compartment items s1 s2 =
+      match items with
+      | [] ->
+          CSet.inter s1 s2 |> CSet.elements |> List.map ~f:priority |> Util.sum
+      | item :: tail ->
+          if compartment >= half
+          then scan_comparment (succ compartment) tail s1 (CSet.add item s2)
+          else scan_comparment (succ compartment) tail (CSet.add item s1) s2
+    in
+    scan_comparment 0 parsed_input CSet.empty CSet.empty
 
-  let%test _ = solve [] = 1
+  let solve input = input |> List.map ~f:solve_aux |> Util.sum
+
+  let%test _ = solve test_input = 157
 end
 
 module B = struct
-  let solve _l = 1
+  let cset_of_string =
+    String.fold_left ~f:(fun s item -> CSet.add item s) ~init:CSet.empty
 
-  let%test _ = solve [] = 1
+  let solve_aux input =
+    let rec scan_rucksacks commons items =
+      match items with
+      | r1 :: r2 :: r3 :: tail ->
+          let s1 = cset_of_string r1 in
+          let s2 = cset_of_string r2 in
+          let s3 = cset_of_string r3 in
+          let common = CSet.inter s1 s2 |> CSet.inter s3 in
+          scan_rucksacks (common :: commons) tail
+      | _ ->
+          commons
+          |> List.map ~f:(CSet.elements %> List.map ~f:priority %> Util.sum)
+          |> Util.sum
+    in
+    scan_rucksacks [] input
+
+  let solve input = input |> solve_aux
+
+  let%test _ = solve test_input = 70
 end
 
 let run () =
-  let input = "./lib/day_3/input" in
+  let input = Util.read_file "./lib/day_3/input" in
 
-  let parsed_input = input |> Util.read_file in
+  let _puzzle_a = input |> A.solve |> Printf.printf "\nDay 3A: %i\n%!" in
 
-  let _puzzle_a = parsed_input |> A.solve |> Printf.printf "\nDay 3A: %i\n%!" in
-
-  let _puzzle_b = parsed_input |> B.solve |> Printf.printf "Day 3B: %i\n%!" in
+  let _puzzle_b = input |> B.solve |> Printf.printf "Day 3B: %i\n%!" in
 
   ()
--- lib/day_4/day_4.ml
+++ lib/day_4/day_4.ml
@@ -1,24 +1,38 @@
 open ContainersLabels
 
-module A = struct
-  let solve _l = 1
+let test_input =
+  [ "2-4,6-8"; "2-3,4-5"; "5-7,7-9"; "2-8,3-7"; "6-6,4-6"; "2-6,4-8" ]
 
-  let%test _ = solve [] = 1
+let parsers =
+  Aoc_2022.Util.[ parse "%i-%i,%i-%i" (fun i j k l -> ((i, j), (k, l))) ]
+
+let parse_instructions = Aoc_2022.Util.try_parse parsers
+let included (i, j) (k, l) = k <= i && j <= l
+let disjoint (i, j) (k, l) = j < k || l < i
+
+module A = struct
+  let solve input =
+    let section_assignment_pairs = List.map ~f:parse_instructions input in
+    let range_included (i, j) = included i j || included j i in
+    List.filter ~f:range_included section_assignment_pairs |> List.length
+
+  let%test _ = solve test_input = 2
 end
 
 module B = struct
-  let solve _l = 1
+  let solve input =
+    let section_assignment_pairs = List.map ~f:parse_instructions input in
+    let range_disjoint (i, j) = not (disjoint i j || disjoint j i) in
+    List.filter ~f:range_disjoint section_assignment_pairs |> List.length
 
-  let%test _ = solve [] = 1
+  let%test _ = solve test_input = 4
 end
 
 let run () =
-  let input = "./lib/day_4/input" in
+  let input = Util.read_file "./lib/day_4/input" in
 
-  let parsed_input = input |> Util.read_file in
+  let _puzzle_a = input |> A.solve |> Printf.printf "\nDay 4A: %i\n%!" in
 
-  let _puzzle_a = parsed_input |> A.solve |> Printf.printf "\nDay 4A: %i\n%!" in
-
-  let _puzzle_b = parsed_input |> B.solve |> Printf.printf "Day 4B: %i\n%!" in
+  let _puzzle_b = input |> B.solve |> Printf.printf "Day 4B: %i\n%!" in
 
   ()
