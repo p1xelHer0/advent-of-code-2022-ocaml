d90e678abbbd03c652140f152d97e7923471801e
Pontus Nagy <p_nagy@icloud.com>
2022-12-07 00:38:35 +0100

    day 6 done, still missing day 5 noooooooo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ✨ Advent of Code 2022 ✨
 
-> 1AB 2AB 3AB 4AB 5-- 6-- 7-- 8-- 9-- 10-- 11-- 12-- 13-- 14-- 15-- 16-- 17-- 18-- 19-- 20-- 21-- 22-- 23-- 24-- 25-- solved
+> 1AB 2AB 3AB 4AB 5-- 6AB 7-- 8-- 9-- 10-- 11-- 12-- 13-- 14-- 15-- 16-- 17-- 18-- 19-- 20-- 21-- 22-- 23-- 24-- 25-- solved
 
 ## My solutions to [Advent of Code 2022](https://adventofcode.com/2022/) written in [OCaml](https://ocaml.org/)
 
--- lib/day_6/day_6.ml
+++ lib/day_6/day_6.ml
@@ -1,24 +1,45 @@
 open ContainersLabels
 
-module A = struct
-  let solve _l = 1
+let test_input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
-  let%test _ = solve [] = 1
+let solve' l distinct_markers =
+  let rec aux input string i =
+    match input with
+    | [] -> failwith "end of file"
+    | c :: tl ->
+        if List.length string >= distinct_markers
+        then i
+        else if List.mem c string
+        then
+          let string' =
+            match
+              string |> Array.of_list |> Array.find_idx ~f:(Char.equal c)
+            with
+            | Some (i, _) -> List.drop (succ i) string @ [ c ]
+            | None -> string
+          in
+          aux tl string' (succ i)
+        else aux tl (string @ [ c ]) (succ i)
+  in
+  let input = String.to_list l in
+  aux input [] 0
+
+module A = struct
+  let solve l = solve' l 4
+
+  let%test _ = solve test_input = 11
 end
 
 module B = struct
-  let solve _l = 1
+  let solve l = solve' l 14
 
-  let%test _ = solve [] = 1
+  let%test _ = solve test_input = 26
 end
 
 let run () =
-  let input = "./lib/day_6/input" in
+  let input = Util.read_file_as_string "./lib/day_6/input" in
 
-  let parsed_input = input |> Util.read_file in
+  let _puzzle_a = input |> A.solve |> Printf.printf "\nDay 6A: %i\n%!" in
 
-  let _puzzle_a = parsed_input |> A.solve |> Printf.printf "\nDay 6A: %i\n%!" in
-
-  let _puzzle_b = parsed_input |> B.solve |> Printf.printf "Day 6B: %i\n%!" in
-
+  let _puzzle_b = input |> B.solve |> Printf.printf "Day 6B: %i\n%!" in
   ()
